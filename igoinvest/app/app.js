524aedacef6f984e65a22f86d0f8b0eab4af3272
D2020 <dreo2020@gmail.com>
2022-08-16 21:57:53 +0800

    mysql code

--- igoinvest/app/app.js
+++ igoinvest/app/app.js
@@ -11,7 +11,7 @@ const userInViews = require('./lib/middleware/userInViews');
 const authRouter = require('./routes/auth');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const con = mysql.createConnection({
   host: "eu-cdbr-west-03.cleardb.net",
