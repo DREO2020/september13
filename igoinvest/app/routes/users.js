cb5149aa0421b69364d101779e19c8531181e207
Nicholas Phang <phang.nicholas@yahoo.com.sg>
2022-09-12 23:09:14 +0800

    Check req in post

--- igoinvest/app/routes/users.js
+++ igoinvest/app/routes/users.js
@@ -50,7 +50,7 @@ function insertNewUser(data) {
 
 router.post("/user/add", function (req, res) {
   console.log("LOOOOOK HERRRRRRE");
-  console.log(res.data.data);
+  console.log(res);
   console.log("REQQQQ");
   console.log(req);
   setTimeout(() => {
