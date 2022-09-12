53228945e2cf78700f1e5e4351a875a744261288
Nicholas Phang <phang.nicholas@yahoo.com.sg>
2022-09-12 14:36:28 +0800

    Initial Commit

--- igoinvest/app/routes/users.js
+++ igoinvest/app/routes/users.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const secured = require('../lib/middleware/secured');
 const router = express.Router();
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.DBHOST,
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWD,
+  database: process.env.DBNAME
+})
+
+connection.connect();
 
 /* GET user profile. */
 router.get('/user', secured(), function (req, res, next) {
@@ -13,21 +26,14 @@ router.get('/user', secured(), function (req, res, next) {
 
 router.post("/user/add", (req, res) => {
   connection.query(
-    `insert into 
-    wallets (user_id, create_time, update_time, content, wallet_sgd_balance, email) 
-    values (
-      2, 
-      NOW(), 
-      NOW(), 
-      "hello",
-      2,
-      "dreo2020@gmail.com")`,
+    `insert into wallets (user_id, create_time, update_time, content, wallet_sgd_balance, email) 
+    values (2, NOW(), NOW(), "hello", 2, "dreo2020@gmail.com")`,
     (errors, results) => {
       if (errors) {
         console.log(errors);
-        response.status(500).send("Some error occurred...");
+        res.status(500).send("Some error occurred...");
       }
-      response.status(200).send("Added a new user!");
+      res.status(200).send("Added a new user!");
     }
   );
 });
