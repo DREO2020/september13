4d3a09f462185bea9424856a433db2a986dada85
Nicholas Phang <phang.nicholas@yahoo.com.sg>
2022-09-12 22:08:34 +0800

    Check req in post

--- igoinvest/app/routes/users.js
+++ igoinvest/app/routes/users.js
@@ -49,7 +49,7 @@ function insertNewUser(data) {
 }
 
 router.post("/user/add", (req, res) => {
-  console.log(req);
+  console.log(req.params);
   setTimeout(() => {
     insertNewUser({
       "user_id": user_id,
